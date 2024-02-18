820c62d1265f8709d3c558b6f1d52a8296ec38a8
M Kuznetcov <michail.kuznetsov@gmail.com>
2024-02-18 20:55:23 +0100

    add functions, geo

---
+++ netlify/edge-functions/geolocation.ts
@@ -0,0 +1,14 @@
+import type { Config, Context } from "@netlify/edge-functions";
+
+
+export default async (request: Request, context: Context) => {
+    return Response.json({
+        geo: context.geo,
+        time: Date.now(),
+        env: process.env.MY_IMPORTANT_VARIABLE
+      });
+    };
+
+export const config: Config = {
+    path: "/geolocation",
+};
---
+++ pages/home/index.js
@@ -0,0 +1,17 @@
+export default {
+    title: "Home",
+    metaDescription:
+      "checking how this can be deployed to netlify",
+    page: function () {
+      return /* html */ `
+      <section class="home__section">
+      <h2>Examples</h2>
+      <p>
+        Explore the examples below, or over in the <a href="https://github.com/netlify/edge-functions-examples" target="_BLANK" rel="noopener">GitHub repository</a> for this site.
+      </p>
+      </section>
+
+
+    `;
+    },
+  };
