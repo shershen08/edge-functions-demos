6f99da01394baeb14328a85b5bc910409e33203d
M Kuznetcov <michail.kuznetsov@gmail.com>
2024-02-18 21:04:42 +0100

    add missing setup

---
+++ netlify/edge-functions/[page].js
@@ -0,0 +1,30 @@
+import pageHome from "../../pages/home/index.js";
+import pageWithFunctions from "../../pages/home/page-with-functions.js";
+const pages = {
+    home: pageHome,
+    functions: pageWithFunctions
+}
+
+export default (request, context) => {
+    const url = new URL(request.url);
+    const path = context.params?.page || "home";
+
+    console.log(`serve page for ${url} `);
+
+    // render the appropriate page with the global layout
+    const html = layout({
+      url: url,
+      title: pages[path].title,
+      content: pages[path].page({ geo: context.geo, env: context.env, time: context.time}),
+      metaDescription: pages[path].metaDescription,
+    });
+
+    // send our response
+    return new Response(html, {
+      headers: { "content-type": "text/html" },
+    });
+  };
+
+export const config = {
+    path: ["/", "/example/:page"],
+  };
